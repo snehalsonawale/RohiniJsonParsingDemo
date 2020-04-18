124a208c9d8cec8cd0b17d48ccbf09fb032f3578
snehalsonawale222 <snehal.sonawale@firstcry.com>
2020-04-18 23:54:37 +0530

    final done demo  commit

--- app/src/main/java/com/example/rohinijsonparsingdemo/RetrofitData.java
+++ app/src/main/java/com/example/rohinijsonparsingdemo/RetrofitData.java
@@ -4,7 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitData {
-    public static final String BASE_URL = "http://stam.attendanceportal.com/8540";
+    //public static final String BASE_URL = "http://stam.attendanceportal.com/8540";
+    public static final String BASE_URL = "";
     public static Retrofit retrofit = null;
 
     public static Retrofit getRetrofit(){
